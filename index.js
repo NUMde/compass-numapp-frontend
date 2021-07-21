b8af1c145ec5b110aaa895c7e99af98cc97dbc49
Alexander Gottschall <alexander.gottschall@de.ibm.com>
2021-07-21 11:11:11 +0200

    added another eventhandler

--- index.js
+++ index.js
@@ -4,8 +4,21 @@ import { AppRegistry } from 'react-native';
 import messaging from '@react-native-firebase/messaging';
 import App from './App';
 import Config from './src/config/appConfig'
+import { Alert } from 'react-native';
 
 // Register background handler
-if(Config.connectToFCM) messaging().setBackgroundMessageHandler(async remoteMessage => true)
+if(Config.connectToFCM) {
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+        Alert.alert("Background Notification","RECEIVED")
+        console.log('Background!', remoteMessage);
+        return true;
+    });
+
+    messaging().onMessage(async remoteMessage => {
+        Alert.alert("Notification","RECEIVED")
+        console.log('Background!', remoteMessage);
+        return true;
+    });
+}
 
 AppRegistry.registerComponent('Compass', () => App);
