197853bfcfc319afe78e35b67e3498d07a46b347
mbastian93 <m_bastian@outlook.de>
2021-08-26 15:39:09 +0200

    fix linting problems in progressbar.jsx

--- src/components/modal/progressbar.jsx
+++ src/components/modal/progressbar.jsx
@@ -1,73 +1,69 @@
-
 /***********************************************************************************************
 import
 ***********************************************************************************************/
 
-import  React, { Component } from 'react'
-import { View, StyleSheet } from "react-native"
+import React, { PureComponent } from "react";
+import { View, StyleSheet } from "react-native";
 
-import config from '../../config/configProvider'
+import config from "../../config/configProvider";
+
+let localStyle;
+
+const calculateWidth = (width) =>
+  StyleSheet.create({
+    progressBarWidth: {
+      width: `${width * 100}%`,
+    },
+  });
 
 /***********************************************************************************************
 component:
 renders a progressbar on the bottom of the questionnaireModal
 ***********************************************************************************************/
 
-class ProgressBar extends Component {
+class ProgressBar extends PureComponent {
+  /**
+   * @constructor
+   * @param {object} props
+   * @param {number} props.progress the progress as a decimal value between 0 and 1
+   */
 
-    /**
-     * @constructor
-     * @param {object} props 
-     * @param {number} props.progress the progress as a decimal value between 0 and 1 
-     */
-    constructor(props) {
-        super(props)
-    }
+  // rendering
+  /*-----------------------------------------------------------------------------------*/
 
-    // rendering
-    /*-----------------------------------------------------------------------------------*/
+  render() {
+    const { progress } = this.props;
+    const width = calculateWidth(progress);
 
-    render() {
-
-        let width = this.setWidth(this.props.progress)
-        
-        return (
-            <View style={localStyle.container}>
-                <View style={{...localStyle.progressBar, ...width.progressBarWidth}}></View>   
-            </View>
-        )
-    }
-
-    setWidth(width) {
-       
-        return StyleSheet.create({
-            progressBarWidth: {
-              width: (width * 100) + '%',
-            }
-        })
-    }
+    return (
+      <View style={localStyle.container}>
+        <View
+          style={{ ...localStyle.progressBar, ...width.progressBarWidth }}
+        />
+      </View>
+    );
+  }
 }
 
 /***********************************************************************************************
 local styling
 ***********************************************************************************************/
 
-const localStyle = StyleSheet.create({
-  
-    container: {
-        width: "95%",
-        alignSelf: "center",
-        height: 5,
-        borderRadius: 2.5,
-        marginHorizontal: 5,
-        backgroundColor: config.theme.colors.accent2
-    },
+localStyle = StyleSheet.create({
+  container: {
+    width: "95%",
+    alignSelf: "center",
+    height: 5,
+    borderRadius: 2.5,
+    marginHorizontal: 5,
+    backgroundColor: config.theme.colors.accent2,
+  },
 
-    progressBar: {
-        height: 5,
-        borderRadius: 2.5,
-        backgroundColor: config.theme.colors.primary
-    }
-})
+  progressBar: {
+    height: 5,
+    borderRadius: 2.5,
+    backgroundColor: config.theme.colors.primary,
+  },
+});
 
-export default ProgressBar
+export default ProgressBar;
