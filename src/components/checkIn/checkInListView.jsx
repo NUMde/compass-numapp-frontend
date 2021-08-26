8ba7dac47b6262180875c5dc9fcafbb23c351e0f
mbastian93 <m_bastian@outlook.de>
2021-08-26 13:59:46 +0200

    fix linting problems in checkInListView.jsx
    also some calculations are move in methods for testability
    as well as maintainability

--- src/components/checkIn/checkInListView.jsx
+++ src/components/checkIn/checkInListView.jsx
@@ -1,23 +1,23 @@
-
 // (C) Copyright IBM Deutschland GmbH 2021.  All rights reserved.
 
 /***********************************************************************************************
 imports
 ***********************************************************************************************/
 
-import  React, { Component } from 'react'
-import { View, StyleSheet } from 'react-native'
-import { ListItem } from 'react-native-elements'
+import React, { PureComponent } from "react";
+import { View, StyleSheet } from "react-native";
+import { ListItem } from "react-native-elements";
 
-import config from '../../config/configProvider'
+import config from "../../config/configProvider";
+
+let localStyle;
 
 /***********************************************************************************************
 component
 ***********************************************************************************************/
 
-class CheckInListView extends Component {
-
-    /**
+class CheckInListView extends PureComponent {
+  /**
     * renders a single ListItem which represents the current state of a loaded questionnaire. 
     * red for untouched, yellow for incomplete and green for completed questionnaires.
     * the state of the questionnaire also impacts wich icon will be rendered on the right-hand-side.
@@ -33,115 +33,142 @@ class CheckInListView extends Component {
     * @param  {boolean}     props.categoriesLoaded true if the questionnaire is ready to be rendered
     * @param  {Function}    props.formatDateString formats a date string
     */
-    constructor(props) {
-        super(props)
-    }
 
-    // rendering
-    /*-----------------------------------------------------------------------------------*/
-
-    render() {
-        return (
-            <View style={localStyle.wrapper}>
-                {/* if all categories are loaded AND there is a current questionnaire available render a single ListLink*/}
-                {
-                    (this.props.categoriesLoaded && !this.props.noNewQuestionnaireAvailableYet )
-                    && 
-                    (<ListItem 
-                        containerStyle={{
-                            ...localStyle.containerStyle,
-                            // if the questionnaire is partially filled out render in yellow
-                            ...(this.props.questionnaireItemMap && !this.props.questionnaireItemMap.done && this.props.questionnaireItemMap.started ? localStyle.containerTouched :
-                                    // if the questionnaire is untouched render in red
-                                    !this.props.questionnaireItemMap.done && !this.props.questionnaireItemMap.started ? localStyle.containerUntouched :
-                                        // if the questionnaire is completed render in green
-                                        localStyle.containerCompleted )
-                        }}
-                        onPress={() => this.props.navigation.navigate('Survey')}
-                        accessibilityLabel={(this.props.firstTime ? config.text.survey.surveyTitleFirstTime : config.text.survey.surveyTitle) + ". " + (config.text.survey.surveySubTitle + this.props.formatDateString(new Date(this.props.user.due_date)))}
-                        accessibilityRole={config.text.accessibility.types.button}
-                        accessibilityHint={config.text.accessibility.questionnaire.questionnaireCellHint + 
-                            ((this.props.questionnaireItemMap && !this.props.questionnaireItemMap.done && this.props.questionnaireItemMap.started) 
-                            ? config.text.accessibility.questionnaire.questionnaire + config.text.accessibility.questionnaire.notFinished 
-                            : (!this.props.questionnaireItemMap.done && !this.props.questionnaireItemMap.started)
-                                ? config.text.accessibility.questionnaire.questionnaire + config.text.accessibility.questionnaire.notStarted 
-                                : (this.props.questionnaireItemMap && this.props.questionnaireItemMap.done)
-                                    ? config.text.accessibility.questionnaire.questionnaire + config.text.accessibility.questionnaire.finished 
-                                    : "")}
-                    >
-                        <ListItem.Content>
-                            {/* shows a special title for first-time-users or the regular title for all other users */}
-                            <ListItem.Title style={localStyle.title}>
-                                {this.props.firstTime ? config.text.survey.surveyTitleFirstTime : config.text.survey.surveyTitle}
-                            </ListItem.Title>
-    
-                            {/* subtitle with formatted due date of the questionnaire */}
-                            <ListItem.Subtitle style={localStyle.subTitle} >
-                                {config.text.survey.surveySubTitle + this.props.formatDateString(new Date(this.props.user.due_date))}
-                            </ListItem.Subtitle>
-                        </ListItem.Content>
-    
-                        {/* renders the yellow icon if the questionnaire is partially completed */}
-                        {
-                            this.props.questionnaireItemMap && !this.props.questionnaireItemMap.done && this.props.questionnaireItemMap.started && 
-                            (<ListItem.Chevron
-                                {
-                                    ...{
-                                        type: 'material-community',
-                                        name: 'dots-horizontal',
-                                        color: config.theme.colors.secondary,
-                                        size: 12,
-                                        raised: true,
-                                        containerStyle: {
-                                            backgroundColor: config.theme.colors.white
-                                        }
-                                    }
-                                }
-                            />
-                        )}
-    
-                        {/* renders the red icon if the questionnaire is untouched */}
-                        {!this.props.questionnaireItemMap.done &&
-                            !this.props.questionnaireItemMap.started && 
-                            (<ListItem.Chevron
-                                {
-                                    ...{
-                                        type: 'material-community',
-                                        name: 'pencil-outline',
-                                        color: config.theme.colors.alert,
-                                        size: 12,
-                                        raised: true,
-                                        containerStyle: {
-                                            backgroundColor: config.theme.colors.white
-                                        }
-                                    }
-                                }
-                            />
-                        )}
-    
-                        {/* renders the green icon if the questionnaire is completed */}
-                        {this.props.questionnaireItemMap && 
-                            this.props.questionnaireItemMap.done && 
-                            (<ListItem.Chevron
-                                {
-                                    ...{
-                                        type: 'material-community',
-                                        name: 'check',
-                                        color: config.theme.colors.success,
-                                        size: 12,
-                                        raised: true,
-                                        containerStyle: {
-                                            backgroundColor: config.theme.colors.white
-                                        }
-                                    }
-                                }
-                            />
-                        )}
-                    </ListItem>)
-                }
-            </View>
-        )
+  /**
+   * depending on the state of the questionnaire (untouched, started, finished),
+   * this method returns a styleSheet object for the list item
+   * @returns a style object for the listItem
+   */
+  getListItemStyle = () => {
+    const { questionnaireItemMap } = this.props;
+    if (
+      questionnaireItemMap &&
+      !questionnaireItemMap.done &&
+      questionnaireItemMap.started
+    ) {
+      return localStyle.containerTouched;
     }
+    if (!questionnaireItemMap.done && !questionnaireItemMap.started) {
+      return localStyle.containerUntouched;
+    }
+    return localStyle.containerCompleted;
+  };
+
+  /**
+   * determine the accessibility hint fot the list item depending on the state of the questionnaire
+   * @returns accessibility hint
+   */
+  getAccessibilityHint = () => {
+    const { questionnaireItemMap } = this.props;
+    let hint =
+      config.text.accessibility.questionnaire.questionnaireCellHint +
+      config.text.accessibility.questionnaire.questionnaire;
+    if (
+      questionnaireItemMap &&
+      !questionnaireItemMap.done &&
+      questionnaireItemMap.started
+    ) {
+      hint += config.text.accessibility.questionnaire.notFinished;
+      return hint;
+    }
+    if (!questionnaireItemMap.done && !questionnaireItemMap.notStarted) {
+      hint += config.text.accessibility.questionnaire.notStarted;
+      return hint;
+    }
+    hint += config.text.accessibility.questionnaire.finished;
+    return hint;
+  };
+
+  /**
+   * determine name and color of the icon displayed on the list item base on the state of the questionnaire
+   * @returns
+   */
+  getChevronProps = () => {
+    const { questionnaireItemMap } = this.props;
+    if (
+      questionnaireItemMap &&
+      !questionnaireItemMap.done &&
+      !questionnaireItemMap.started
+    )
+      return {
+        name: "dots-horizontal",
+        color: config.theme.colors.secondary,
+      };
+
+    if (questionnaireItemMap.started && !questionnaireItemMap.done)
+      return {
+        name: "pencil-outline",
+        color: config.theme.colors.alert,
+      };
+    if (questionnaireItemMap.done)
+      return {
+        name: "check",
+        color: config.theme.colors.success,
+      };
+    return {};
+  };
+
+  // rendering
+  /*-----------------------------------------------------------------------------------*/
+
+  render() {
+    const {
+      categoriesLoaded,
+      noNewQuestionnaireAvailableYet,
+      navigation,
+      formatDateString,
+      user,
+    } = this.props;
+    return (
+      <View style={localStyle.wrapper}>
+        {/* if all categories are loaded AND there is a current questionnaire available render a single ListLink*/}
+        {categoriesLoaded && !noNewQuestionnaireAvailableYet && (
+          <ListItem
+            containerStyle={{
+              ...localStyle.containerStyle,
+              // get additional styling depending on the state of the questionnaire
+              ...this.getListItemStyle(),
+            }}
+            onPress={() => navigation.navigate("Survey")}
+            accessibilityLabel={`${
+              user.firstTime
+                ? config.text.survey.surveyTitleFirstTime
+                : config.text.survey.surveyTitle
+            }. ${
+              config.text.survey.surveySubTitle +
+              formatDateString(new Date(user.due_date))
+            }`}
+            accessibilityRole={config.text.accessibility.types.button}
+            accessibilityHint={this.getAccessibilityHint()}
+          >
+            <ListItem.Content>
+              {/* shows a special title for first-time-users or the regular title for all other users */}
+              <ListItem.Title style={localStyle.title}>
+                {user.firstTime
+                  ? config.text.survey.surveyTitleFirstTime
+                  : config.text.survey.surveyTitle}
+              </ListItem.Title>
+
+              {/* subtitle with formatted due date of the questionnaire */}
+              <ListItem.Subtitle style={localStyle.subTitle}>
+                {config.text.survey.surveySubTitle +
+                  formatDateString(new Date(user.due_date))}
+              </ListItem.Subtitle>
+            </ListItem.Content>
+            {/* renders icon */}
+            <ListItem.Chevron
+              type="material-community"
+              size={12}
+              raised
+              containerStyle={{ backgroundColor: config.theme.colors.white }}
+              // get additional properties based on the state of the questionnaire
+              iconProps={this.getChevronProps()}
+            />
+          </ListItem>
+        )}
+      </View>
+    );
+  }
 }
 
 /***********************************************************************************************
@@ -151,49 +178,51 @@ local styling
 // scaleUiFkt() (located in src/config/appConfig.js)
 // will dynamically alter some sized based on the physical device-measurements.
 
-const localStyle = StyleSheet.create({
-	wrapper: {
-        marginBottom: config.appConfig.scaleUiFkt(30),
-    },
-    
-    containerStyle: {
-        width: '100%',
-        borderBottomWidth: 1,
-        borderTopWidth: 1,
-        padding: config.appConfig.scaleUiFkt(30),
-    },
-    
-    title: {
-        ...config.theme.fonts.title2,
-        color: config.theme.values.defaultCheckInListViewTitleColor
-    },  
+localStyle = StyleSheet.create({
+  wrapper: {
+    marginBottom: config.appConfig.scaleUiFkt(30),
+  },
 
-    subTitle: {
-        color: config.theme.values.defaultCheckInListViewSubTitleColor,
-        ...config.theme.fonts.body,
-    },
+  containerStyle: {
+    width: "100%",
+    borderBottomWidth: 1,
+    borderTopWidth: 1,
+    padding: config.appConfig.scaleUiFkt(30),
+  },
 
-    containerUntouched: {
-        borderBottomColor: config.theme.values.defaultContainerUntouchedBorderColor,
-        borderTopColor: config.theme.values.defaultContainerUntouchedBorderColor,
-        backgroundColor: config.theme.values.defaultContainerUntouchedBackgroundColor,
-    },
+  title: {
+    ...config.theme.fonts.title2,
+    color: config.theme.values.defaultCheckInListViewTitleColor,
+  },
 
-    containerTouched: {
-        borderBottomColor: config.theme.values.defaultContainerTouchedBorderColor,
-        borderTopColor: config.theme.values.defaultContainerTouchedBorderColor,
-        backgroundColor: config.theme.values.defaultContainerTouchedBackgroundColor,
-    },
+  subTitle: {
+    color: config.theme.values.defaultCheckInListViewSubTitleColor,
+    ...config.theme.fonts.body,
+  },
 
-    containerCompleted: {
-        borderBottomColor: config.theme.values.defaultContainerCompletedBorderColor,
-        borderTopColor: config.theme.values.defaultContainerCompletedBorderColor,
-        backgroundColor: config.theme.values.defaultContainerCompletedBackgroundColor,
-    }
-})
+  containerUntouched: {
+    borderBottomColor: config.theme.values.defaultContainerUntouchedBorderColor,
+    borderTopColor: config.theme.values.defaultContainerUntouchedBorderColor,
+    backgroundColor:
+      config.theme.values.defaultContainerUntouchedBackgroundColor,
+  },
+
+  containerTouched: {
+    borderBottomColor: config.theme.values.defaultContainerTouchedBorderColor,
+    borderTopColor: config.theme.values.defaultContainerTouchedBorderColor,
+    backgroundColor: config.theme.values.defaultContainerTouchedBackgroundColor,
+  },
+
+  containerCompleted: {
+    borderBottomColor: config.theme.values.defaultContainerCompletedBorderColor,
+    borderTopColor: config.theme.values.defaultContainerCompletedBorderColor,
+    backgroundColor:
+      config.theme.values.defaultContainerCompletedBackgroundColor,
+  },
+});
 
 /***********************************************************************************************
 export
 ***********************************************************************************************/
 
-export default CheckInListView
+export default CheckInListView;
