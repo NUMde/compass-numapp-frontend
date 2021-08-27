c80a905adb953dee7f16a7bca22646e753ca44af
mbastian93 <m_bastian@outlook.de>
2021-08-27 11:47:43 +0200

    fix linting problems in scrollIndicatorWrapper.jsx

--- src/components/scrollIndicatorWrapper/scrollIndicatorWrapper.jsx
+++ src/components/scrollIndicatorWrapper/scrollIndicatorWrapper.jsx
@@ -1,186 +1,210 @@
-
 // (C) Copyright IBM Deutschland GmbH 2021.  All rights reserved.
 
 /***********************************************************************************************
 imports
 ***********************************************************************************************/
 
-import React, { Component } from 'react'
-import { Icon } from 'react-native-elements'
-import { AccessibilityInfo, StyleSheet, TouchableOpacity, Platform, Dimensions, ScrollView} from 'react-native'
+import React, { Component } from "react";
+import { Icon } from "react-native-elements";
+import {
+  AccessibilityInfo,
+  StyleSheet,
+  TouchableOpacity,
+  Platform,
+  Dimensions,
+  ScrollView,
+  KeyboardAvoidingView,
+} from "react-native";
 
-import config from '../../config/configProvider'
-import { KeyboardAvoidingView } from 'react-native'
+import config from "../../config/configProvider";
+
+let localStyle;
 
 /***********************************************************************************************
 component
 ***********************************************************************************************/
 
 export default class ScrollIndicatorWrapper extends Component {
+  // public members
+  /*-----------------------------------------------------------------------------------*/
 
-	/**
-	* adds a scroll-indicator button at the bottom of a ScrollView. this can be de-activated by
-	* manipulating the property 'allowScrollIndicators' in src/config/developmentConfig.js
-	* @constructor
-	* @param  {object}      props
-	* @param  {JSX.Element} props.contentData react-native content to be rendered
-	*/
-	constructor(props) {
-		super(props)
-		this.showIndicator = false
-		this.scrollViewRef = React.createRef()
-		// hides the accessibility indicator when voice over is activated
-		AccessibilityInfo.isScreenReaderEnabled().then(
-			screenReaderEnabled => {
-				this.isAccessibilityOn = screenReaderEnabled
-			}
-		)
-	}
+  /**
+   * used to persist the state of the indicator (show/no show)
+   * @type {boolean}
+   */
+  showIndicator;
 
-	// public members
-	/*-----------------------------------------------------------------------------------*/
+  /**
+   * reference for the scrollView component
+   * @type {object}
+   */
+  scrollViewRef;
 
-	/**
-	* used to persist the state of the indicator (show/no show)
-	* @type {boolean}
-	*/
-	showIndicator
+  /**
+   * tells us if the screen reader is enabled
+   * @type {boolean}
+   */
+  isAccessibilityOn;
 
-	/**
-	* reference for the scrollView component
-	* @type {object}
-	*/
-	scrollViewRef
+  /**
+   * holds the position where to scroll next (when hitting the scrollIndicator-Button)
+   * @type {number}
+   */
+  nextScrollValue;
 
-	/**
-	* tells us if the screen reader is enabled
-	* @type {boolean}
-	*/
-	isAccessibilityOn
+  /**
+   * adds a scroll-indicator button at the bottom of a ScrollView. this can be de-activated by
+   * manipulating the property 'allowScrollIndicators' in src/config/developmentConfig.js
+   * @constructor
+   * @param  {object}      props
+   * @param  {JSX.Element} props.contentData react-native content to be rendered
+   */
+  constructor(props) {
+    super(props);
+    this.showIndicator = false;
+    this.scrollViewRef = React.createRef();
+    // hides the accessibility indicator when voice over is activated
+    AccessibilityInfo.isScreenReaderEnabled().then((screenReaderEnabled) => {
+      this.isAccessibilityOn = screenReaderEnabled;
+    });
+  }
 
-	/**
-	* holds the position where to scroll next (when hitting the scrollIndicator-Button)
-	* @type {number}
-	*/
-	nextScrollValue
+  // class events
+  /*-----------------------------------------------------------------------------------*/
 
-	// rendering
-	/*-----------------------------------------------------------------------------------*/
+  /**
+   * just executes the scroll-event after the component mounted (so that the indicator
+   * will be rendered)
+   */
+  componentDidMount = () => this.scrollElement();
 
-	render() {
-		return (
-			<KeyboardAvoidingView style={localStyle.wrapper}>
-				{/* the regular ScrollView & content */}
-				<ScrollView
-					{...this.props}
-					enableOnAndroid={true}
-					ref={this.scrollViewRef}
-					style={localStyle.wrapper}
-					innerRef={ref => this.scroll = ref}
-					contentContainerStyle={localStyle.contentContainerStyle}
-					onScroll={(event) => {
-						// calculating the next scroll-position
-						let bannerHeight = Platform.OS === "ios" ? config.appConfig.scaleUiFkt(290, 0.7) : config.appConfig.scaleUiFkt(290, 0.6)
-						let contentHeight = Dimensions.get('window').height - bannerHeight
-						this.nextScrollValue = event.nativeEvent.contentOffset.y + contentHeight - localStyle.indicator.height - localStyle.indicator.bottom - 20
-						
-						// determines if the indicator should be displayed or not
-						this.showIndicator =
-						event.nativeEvent.layoutMeasurement.height < event.nativeEvent.contentSize.height &&
-						event.nativeEvent.contentSize.height - event.nativeEvent.layoutMeasurement.height - event.nativeEvent.contentOffset.y > 30
+  // modal events
+  /*-----------------------------------------------------------------------------------*/
 
-						// applies the updates
-						this.forceUpdate()
-					}}
-				>
-					{/* the content */}
-					{this.props.contentData}
-				</ScrollView>
+  /**
+   * basically, a scroll-to-top function
+   */
+  scrollElement() {
+    window.requestAnimationFrame(() => {
+      const node = this.scrollViewRef;
+      if (node !== undefined) {
+        if (node.current) node.current.scrollTo({ x: 0, y: 1, animated: true });
+      }
+    });
+  }
 
-				{/* if "allowScrollIndicators" is true and the situation calls for a scroll indicator ("showIndicator") and
+  // rendering
+  /*-----------------------------------------------------------------------------------*/
+
+  render() {
+    const { contentData } = this.props;
+    return (
+      <KeyboardAvoidingView style={localStyle.wrapper}>
+        {/* the regular ScrollView & content */}
+        <ScrollView
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          {...this.props}
+          enableOnAndroid
+          ref={this.scrollViewRef}
+          style={localStyle.wrapper}
+          innerRef={(ref) => (this.scroll = ref)}
+          contentContainerStyle={localStyle.contentContainerStyle}
+          onScroll={(event) => {
+            // calculating the next scroll-position
+            const bannerHeight =
+              Platform.OS === "ios"
+                ? config.appConfig.scaleUiFkt(290, 0.7)
+                : config.appConfig.scaleUiFkt(290, 0.6);
+            const contentHeight =
+              Dimensions.get("window").height - bannerHeight;
+            this.nextScrollValue =
+              event.nativeEvent.contentOffset.y +
+              contentHeight -
+              localStyle.indicator.height -
+              localStyle.indicator.bottom -
+              20;
+
+            // determines if the indicator should be displayed or not
+            this.showIndicator =
+              event.nativeEvent.layoutMeasurement.height <
+                event.nativeEvent.contentSize.height &&
+              event.nativeEvent.contentSize.height -
+                event.nativeEvent.layoutMeasurement.height -
+                event.nativeEvent.contentOffset.y >
+                30;
+
+            // applies the updates
+            this.forceUpdate();
+          }}
+        >
+          {/* the content */}
+          {contentData}
+        </ScrollView>
+
+        {/* if "allowScrollIndicators" is true and the situation calls for a scroll indicator ("showIndicator") and
 				we are not in accessibility-mode */}
-				{
-					(config.theme.ui.allowScrollIndicators && this.showIndicator && !this.isAccessibilityOn )
-					&& 
-					(<TouchableOpacity
-						accessibilityRole={config.text.accessibility.types.button}
-						style={localStyle.indicator}
-						onPress={()=>{
-							this.scrollViewRef.current.scrollTo({x: 0, y: this.nextScrollValue, animated: true})
-						}}
-					>
-						<Icon
-							name='chevron-down'
-							type='font-awesome'
-							color={config.theme.values.defaultScrollIndicatorIconColor}
-							size={15}
-						/>
-					</TouchableOpacity>)
-				}
-			</KeyboardAvoidingView>
-		)
-	}
-
-	// class events
-	/*-----------------------------------------------------------------------------------*/
-	
-	/**
-	 * just executes the scroll-event after the component mounted (so that the indicator
-	 * will be rendered)
-	 */
-	componentDidMount = () => this.scrollElement()
-
-	// modal events
-	/*-----------------------------------------------------------------------------------*/
-
-	/**
-	 * basically, a scroll-to-top function
-	 */
-	scrollElement() {
-		window.requestAnimationFrame(() => {
-			let node = this.scrollViewRef
-			if (node !== undefined) {
-				if (node.current) node.current.scrollTo({x: 0, y: 1, animated: true})
-			}
-		})
-	}
+        {config.theme.ui.allowScrollIndicators &&
+          this.showIndicator &&
+          !this.isAccessibilityOn && (
+            <TouchableOpacity
+              accessibilityRole={config.text.accessibility.types.button}
+              style={localStyle.indicator}
+              onPress={() => {
+                this.scrollViewRef.current.scrollTo({
+                  x: 0,
+                  y: this.nextScrollValue,
+                  animated: true,
+                });
+              }}
+            >
+              <Icon
+                name="chevron-down"
+                type="font-awesome"
+                color={config.theme.values.defaultScrollIndicatorIconColor}
+                size={15}
+              />
+            </TouchableOpacity>
+          )}
+      </KeyboardAvoidingView>
+    );
+  }
 }
 
 /***********************************************************************************************
 localStyle
 ***********************************************************************************************/
 
-const localStyle = StyleSheet.create({
+localStyle = StyleSheet.create({
+  contentContainerStyle: {
+    flexGrow: 1,
+  },
 
-	contentContainerStyle:{ 
-		flexGrow: 1 
-	},
+  container: {
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "blue",
+    overflow: "hidden",
+  },
 
-	container: {
-		width: '100%',
-		height: '100%',
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: 'blue',
-		overflow: 'hidden',
-	},
+  wrapper: {
+    flex: 1,
+    flexDirection: "column",
+    width: "100%",
+  },
 
-	wrapper: {
-		flex: 1,
-		flexDirection: 'column',
-		width: '100%',
-	},
-
-	indicator: {
-		backgroundColor: config.theme.values.defaultScrollIndicatorBackgroundColor,
-		width: config.appConfig.scaleUiFkt(75),
-		height: config.appConfig.scaleUiFkt(75),
-		position: 'absolute',
-		borderRadius: 100,
-		left: (Dimensions.get('window').width / 2) - (config.appConfig.scaleUiFkt(75) / 2),
-		bottom: 20,
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center'
-	}
-})
+  indicator: {
+    backgroundColor: config.theme.values.defaultScrollIndicatorBackgroundColor,
+    width: config.appConfig.scaleUiFkt(75),
+    height: config.appConfig.scaleUiFkt(75),
+    position: "absolute",
+    borderRadius: 100,
+    left:
+      Dimensions.get("window").width / 2 - config.appConfig.scaleUiFkt(75) / 2,
+    bottom: 20,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
