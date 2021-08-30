3e495f32a7db6033fc0531472798bc56b12357d2
mbastian93 <m_bastian@outlook.de>
2021-08-30 10:52:26 +0200

    fix linting problems in webViewScreen.jsx

--- src/screens/about/webViewScreen.jsx
+++ src/screens/about/webViewScreen.jsx
@@ -1,82 +1,77 @@
-
 // (C) Copyright IBM Deutschland GmbH 2021.  All rights reserved.
 
 /***********************************************************************************************
 imports
 ***********************************************************************************************/
 
-import React, { Component } from 'react'
-import { WebView } from 'react-native-webview'
-import { View, StyleSheet } from 'react-native'
+import React, { PureComponent } from "react";
+import { WebView } from "react-native-webview";
+import { View, StyleSheet } from "react-native";
 
-import store from '../../store'
-import config from '../../config/configProvider'
-import Banner from '../../components/banner/banner'
+import store from "../../store";
+import config from "../../config/configProvider";
+import Banner from "../../components/banner/banner";
+
+let localStyle;
 
 /***********************************************************************************************
 component:
 renders the webView screen
 ***********************************************************************************************/
 
-class WebViewScreen extends Component {
-	
-	/**
-	* @constructor
-	* @param  {object}    props
-	* @param  {object}    props.navigation the navigation object provided by 'react-navigation'
-	*/
-	constructor(props) {
-		super(props)
-	}
+class WebViewScreen extends PureComponent {
+  /**
+   * @constructor
+   * @param  {object}    props
+   * @param  {object}    props.navigation the navigation object provided by 'react-navigation'
+   */
 
-	// rendering
-	/*-----------------------------------------------------------------------------------*/
-	
-	render() {
+  // rendering
+  /*-----------------------------------------------------------------------------------*/
 
-		return(
-			<View>
-				<View style={localStyle.wrapper}>
+  render() {
+    const { navigation } = this.props;
+    return (
+      <View>
+        <View style={localStyle.wrapper}>
+          {/* banner */}
+          <Banner
+            nav={navigation}
+            title={store.getState().About.currentWebView.title}
+            subTitle={store.getState().About.currentWebView.screenSubTitle}
+          />
 
-					{/* banner */}
-					<Banner
-						nav={this.props.navigation}
-						title={store.getState().About.currentWebView.title}
-						subTitle={store.getState().About.currentWebView.screenSubTitle}
-					/>
-
-					{/* content */}
-					<View style={{ ...localStyle.flexi, ...localStyle.wrapper }}>
-						<WebView
-							originWhitelist={['*']}
-							source={{ uri: store.getState().About.currentWebView.uri }}
-						/>
-					</View>
-				</View>
-			</View>
-		)
-	}
+          {/* content */}
+          <View style={{ ...localStyle.flexi, ...localStyle.wrapper }}>
+            <WebView
+              originWhitelist={["*"]}
+              source={{ uri: store.getState().About.currentWebView.uri }}
+            />
+          </View>
+        </View>
+      </View>
+    );
+  }
 }
-
 
 /***********************************************************************************************
 localStyle
 ***********************************************************************************************/
 
-const localStyle = StyleSheet.create({
-	wrapper: {
-		height: '100%',
-		flexDirection: 'column',
-		backgroundColor: config.theme.values.defaultBackgroundColor
-	},
+localStyle = StyleSheet.create({
+  wrapper: {
+    height: "100%",
+    flexDirection: "column",
+    backgroundColor: config.theme.values.defaultBackgroundColor,
+  },
 
-	flexi: {
-		flex: 1
-	}
-})
+  flexi: {
+    flex: 1,
+  },
+});
 
 /***********************************************************************************************
 export
 ***********************************************************************************************/
 
-export default WebViewScreen
+export default WebViewScreen;
