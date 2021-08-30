ecb11ac464322e065321af5414da0d4796877df4
mbastian93 <m_bastian@outlook.de>
2021-08-30 13:45:21 +0200

    fix linting problems in aboutScreen.jsx

--- src/screens/about/aboutScreen.jsx
+++ src/screens/about/aboutScreen.jsx
@@ -1,209 +1,217 @@
-
 // (C) Copyright IBM Deutschland GmbH 2021.  All rights reserved.
 
 /***********************************************************************************************
 imports
 ***********************************************************************************************/
 
-import  React, { Component } from 'react'
-import { View, TouchableOpacity, Text, StyleSheet } from 'react-native'
+import React, { PureComponent } from "react";
+import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
 
-import RedirectModal from '../../components/modal/redirectModal'
-import Banner from '../../components/banner/banner'
-import { ListItem } from 'react-native-elements'
-import AboutListItem from '../../components/about/aboutListItem'
-import AboutListLink from '../../components/about/aboutListLink'
-import ScrollIndicatorWrapper from '../../components/scrollIndicatorWrapper/scrollIndicatorWrapper'
+import { ListItem } from "react-native-elements";
+import RedirectModal from "../../components/modal/redirectModal";
+import Banner from "../../components/banner/banner";
+import AboutListItem from "../../components/about/aboutListItem";
+import AboutListLink from "../../components/about/aboutListLink";
+import ScrollIndicatorWrapper from "../../components/scrollIndicatorWrapper/scrollIndicatorWrapper";
 
-import config from '../../config/configProvider'
+import config from "../../config/configProvider";
+
+let localStyle;
 
 /***********************************************************************************************
 Component
 ***********************************************************************************************/
 
-class AboutScreen extends Component {
+class AboutScreen extends PureComponent {
+  /**
+   * renders the About-Screen
+   * @constructor
+   * @param  {object}    props
+   * @param  {object}    props.navigation the navigation object provided by 'react-navigation'
+   * @param  {function}  props.logout logs out the user
+   * @param  {function}  props.clearAll deletes all local data
+   */
 
-	/**
-	* renders the About-Screen
-	* @constructor
-	* @param  {object}    props
-	* @param  {object}    props.navigation the navigation object provided by 'react-navigation'
-	* @param  {function}  props.logout logs out the user
-	* @param  {function}  props.clearAll deletes all local data
-	*/
-    constructor(props) {
-        super(props)
-    }
+  // rendering
+  /*-----------------------------------------------------------------------------------*/
 
-    // rendering
-    /*-----------------------------------------------------------------------------------*/
+  render() {
+    const { navigation, actions, clearAll, logout, showModal, modalLink } =
+      this.props;
+    return (
+      <View style={localStyle.wrapper}>
+        {/* top banner */}
+        <Banner
+          nav={navigation}
+          title={config.text.about.title}
+          subTitle={config.text.about.subTitle}
+          noMenu
+        />
 
-    render() {
-		return (
-			<View style={localStyle.wrapper}>
-				{/* top banner */}
-				<Banner
-					nav={this.props.navigation}
-					title={config.text.about.title}
-					subTitle={config.text.about.subTitle}
-					noMenu
-				/>
+        {/* the modal to be opened */}
+        <RedirectModal
+          showModal={showModal}
+          modalLink={modalLink}
+          actions={actions}
+        />
 
-				{/* the modal to be opened */}
-				<RedirectModal {...this.props}></RedirectModal>
+        {/* ScrollView with content */}
+        <View style={localStyle.wrapper}>
+          <ScrollIndicatorWrapper
+            contentData={
+              <View style={localStyle.wrapper}>
+                {/* holds the list-items */}
+                <View style={localStyle.wrapper}>
+                  {/* links to the LegalInformationScreen */}
+                  {config.appConfig.allowAccessToLegalInformationScreen && (
+                    <ListItem
+                      containerStyle={localStyle.containerStyle}
+                      onPress={() => navigation.navigate("LegalInformation")}
+                    >
+                      {/* title & subtitle of the listItem - the strings a identified by the webView*/}
+                      <ListItem.Content>
+                        <ListItem.Title style={localStyle.title}>
+                          {config.text.about.legal.title}
+                        </ListItem.Title>
 
-				{/* ScrollView with content */}
-				<View style={localStyle.wrapper}>
-					<ScrollIndicatorWrapper
-						contentData={
-							<View style={localStyle.wrapper}>
-								{/* holds the list-items */}
-								<View style={localStyle.wrapper}>
+                        <ListItem.Subtitle style={localStyle.subTitle}>
+                          {config.text.about.legal.subTitle}
+                        </ListItem.Subtitle>
+                      </ListItem.Content>
 
-									{/* links to the LegalInformationScreen */}
-									{config.appConfig.allowAccessToLegalInformationScreen && (
-										<ListItem 
-											containerStyle={localStyle.containerStyle}
-											onPress={() => this.props.navigation.navigate("LegalInformation")}
-										>
-											{/* title & subtitle of the listItem - the strings a identified by the webView*/}
-											<ListItem.Content>
-												<ListItem.Title style={localStyle.title}>
-													{config.text.about.legal.title}
-												</ListItem.Title>
+                      {/* the icon on the right-hand-side */}
+                      <ListItem.Chevron
+                        {...{
+                          type: config.text.about.legal.iconType,
+                          name: config.text.about.legal.iconTitle,
+                          color: config.theme.values.legalListLinkIconColor,
+                          reverse: true,
+                          size: 12,
+                        }}
+                      />
 
-												<ListItem.Subtitle style={localStyle.subTitle}>
-													{config.text.about.legal.subTitle}
-												</ListItem.Subtitle>
-											</ListItem.Content>
+                      {/* the icon on the right-hand-side */}
+                    </ListItem>
+                  )}
 
-											{/* the icon on the right-hand-side */}
-											<ListItem.Chevron
-												{
-													...{
-														type: config.text.about.legal.iconType,
-														name: config.text.about.legal.iconTitle,
-														color: config.theme.values.legalListLinkIconColor,
-														reverse: true,
-														size: 12,
-													}
-												}
-											/>
+                  {/* iterates over all items in config.text.wevViews */}
+                  {config.text.webViews.map((webView) => (
+                    // navigates to the webview screen
+                    <AboutListLink
+                      key={webView.title}
+                      // {...this.props}
+                      actions={actions}
+                      navigation={navigation}
+                      webView={webView}
+                    />
+                  ))}
 
-											{/* the icon on the right-hand-side */}
-											
-										</ListItem>
-									)}
-									
-									{/* iterates over all items in config.text.wevViews */}
-									{config.text.webViews.map((webView, index) => 
-										// navigates to the webview screen
-										<AboutListLink key={webView.title} {...this.props} navigate webView={webView}/>		
-									)}
+                  {/* iterates over all items in config.text.modalLinks */}
+                  {config.text.modalLinks.map((_modalLink) => (
+                    // navigates to the webview screen
+                    <AboutListItem
+                      actions={actions}
+                      key={_modalLink.title}
+                      modalLink={_modalLink}
+                    />
+                  ))}
+                </View>
 
-									{/* iterates over all items in config.text.modalLinks */}
-									{config.text.modalLinks.map((modalLink, index) => 
-										// navigates to the webview screen
-										<AboutListItem {...this.props} key={modalLink.title} modalLink={modalLink}/>
-									)}
-								</View>
+                {/* optional buttons on the bottom of the screen - JUST FOR DEVELOPMENT*/}
+                <View style={localStyle.bottom}>
+                  {/* logout button */}
+                  {config.appConfig.showLogout && (
+                    <TouchableOpacity
+                      style={localStyle.button}
+                      onPress={logout}
+                      accessibilityLabel={config.text.about.logout}
+                      accessibilityRole={config.text.accessibility.types.button}
+                      accessibilityHint={config.text.accessibility.logoutHint}
+                    >
+                      <Text style={localStyle.buttonLabel}>
+                        {config.text.about.logout}
+                      </Text>
+                    </TouchableOpacity>
+                  )}
 
-								{/* optional buttons on the bottom of the screen - JUST FOR DEVELOPMENT*/}
-								<View style={localStyle.bottom}>
-									{/* logout button */}
-									{config.appConfig.showLogout && (
-										<TouchableOpacity
-											style={localStyle.button}
-											onPress={this.props.logout}
-											accessibilityLabel={config.text.about.logout}
-											accessibilityRole={config.text.accessibility.types.button}
-											accessibilityHint={config.text.accessibility.logoutHint}
-										>
-											<Text style={localStyle.buttonLabel}>
-												{config.text.about.logout}
-											</Text>
-										</TouchableOpacity>	
-									)}
-
-									{/* delete-all-data button */}
-									{config.appConfig.showEraseAll && (
-										<TouchableOpacity
-											style={localStyle.buttonAlert}
-											onPress={this.props.clearAll}
-											accessibilityLabel={config.text.about.delete}
-											accessibilityRole={config.text.accessibility.types.button}
-											accessibilityHint={config.text.accessibility.logoutHint}
-										>
-											<Text style={localStyle.buttonLabel}>
-											{config.text.about.delete}
-											</Text>
-										</TouchableOpacity>	
-									)}
-								</View>
-							</View>
-						}
-					/>
-				</View>
-			</View>
-		)
-	}
+                  {/* delete-all-data button */}
+                  {config.appConfig.showEraseAll && (
+                    <TouchableOpacity
+                      style={localStyle.buttonAlert}
+                      onPress={clearAll}
+                      accessibilityLabel={config.text.about.delete}
+                      accessibilityRole={config.text.accessibility.types.button}
+                      accessibilityHint={config.text.accessibility.logoutHint}
+                    >
+                      <Text style={localStyle.buttonLabel}>
+                        {config.text.about.delete}
+                      </Text>
+                    </TouchableOpacity>
+                  )}
+                </View>
+              </View>
+            }
+          />
+        </View>
+      </View>
+    );
+  }
 }
 
 /***********************************************************************************************
 local styling
 ***********************************************************************************************/
 
-const localStyle = StyleSheet.create({
-	
-	wrapper: {
-		flex: 1,
-		flexDirection: 'column',
-		alignItems: 'center',
-		width: '100%',
-		height: '100%',
-		backgroundColor: config.theme.values.defaultBackgroundColor
-	},
+localStyle = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
+    backgroundColor: config.theme.values.defaultBackgroundColor,
+  },
 
-	bottom: {
-		flex: 1,
-		justifyContent: 'flex-end',
-		marginBottom: 36,
-		height: '100%',
-		marginTop: 20,
-		width: '80%'
-	},
-	
-	button: {
-		...config.theme.classes.buttonPrimary,
-		bottom: 0,
-		marginTop: 10,
-	},
-	
-	buttonAlert: {
-		...config.theme.classes.buttonAlert,
-		bottom: 0,
-		marginTop: 10,
-	},
-	
-	buttonLabel: {
-		...config.theme.classes.buttonLabel
-	},
+  bottom: {
+    flex: 1,
+    justifyContent: "flex-end",
+    marginBottom: 36,
+    height: "100%",
+    marginTop: 20,
+    width: "80%",
+  },
 
-	containerStyle: {
-		width: '100%',
-		borderBottomColor: config.theme.colors.accent3,
-		borderBottomWidth: 1,
-		backgroundColor: config.theme.values.defaultListLinkBackgroundColor
-	},
+  button: {
+    ...config.theme.classes.buttonPrimary,
+    bottom: 0,
+    marginTop: 10,
+  },
 
-	subTitle: {
-		color: config.theme.colors.accent4,
-		...config.theme.fonts.body	
-	},
+  buttonAlert: {
+    ...config.theme.classes.buttonAlert,
+    bottom: 0,
+    marginTop: 10,
+  },
 
-	title: {
-		...config.theme.fonts.title2
-	}
-})
+  buttonLabel: {
+    ...config.theme.classes.buttonLabel,
+  },
 
-export default AboutScreen
+  containerStyle: {
+    width: "100%",
+    borderBottomColor: config.theme.colors.accent3,
+    borderBottomWidth: 1,
+    backgroundColor: config.theme.values.defaultListLinkBackgroundColor,
+  },
+
+  subTitle: {
+    color: config.theme.colors.accent4,
+    ...config.theme.fonts.body,
+  },
+
+  title: {
+    ...config.theme.fonts.title2,
+  },
+});
+
+export default AboutScreen;
