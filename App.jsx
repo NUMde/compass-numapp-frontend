d3acfd1beecf30eb9b151388878fff86a306199f
mbastian93 <m_bastian@outlook.de>
2021-08-31 16:37:40 +0200

    fix linting problems in App.jsx

--- App.jsx
+++ App.jsx
@@ -1,61 +1,59 @@
-
 // (C) Copyright IBM Deutschland GmbH 2021.  All rights reserved.
 
 /***********************************************************************************************
 imports
 ***********************************************************************************************/
 
-import { Provider } from 'react-redux'
-import React, { Component } from 'react'
-import SplashScreen from 'react-native-splash-screen'
-import { StyleSheet, View, StatusBar, LogBox, Platform } from 'react-native'
+import { Provider } from "react-redux";
+import React, { PureComponent } from "react";
+import SplashScreen from "react-native-splash-screen";
+import { StyleSheet, View, StatusBar, LogBox, Platform } from "react-native";
 
-import config from './src/config/configProvider'
+import config from "./src/config/configProvider";
 
-import reduxStore from './src/store'
-import createAppNavigator from './src/navigation/appNavigator'
+import reduxStore from "./src/store";
+import createAppNavigator from "./src/navigation/appNavigator";
 
 /***********************************************************************************************
 Component
 ***********************************************************************************************/
 
-class App extends Component {
+class App extends PureComponent {
+  /**
+   * creates the appNavigator and provides the initial view
+   * @constructor
+   * @param  {object} props
+   */
 
-	/**
-	 * creates the appNavigator and provides the initial view
-	 * @constructor
-	 * @param  {object} props
-	 */	
-    constructor(props) {
-        super(props)
-	}
-	
-	render() {
-		
-		// hides the splash screen
-		SplashScreen.hide()
+  render() {
+    // hides the splash screen
+    SplashScreen.hide();
 
-		// creates the navigator
-		const Navigator = createAppNavigator()
-		
-		// and returns the basic view that contains the navigator
-		return (
-			<View style={localStyle.container}>
+    // creates the navigator
+    const Navigator = createAppNavigator();
 
-				{Platform.OS === "ios" && (
-					<StatusBar barStyle={config.theme.values.defaultStatusBarStyleIos} />
-				)}
+    // and returns the basic view that contains the navigator
+    return (
+      <View style={localStyle.container}>
+        {Platform.OS === "ios" && (
+          <StatusBar barStyle={config.theme.values.defaultStatusBarStyleIos} />
+        )}
 
-				{Platform.OS === "android" && (
-					<StatusBar barStyle={config.theme.values.defaultStatusBarStyleAndroid}  backgroundColor={config.theme.values.defaultStatusBarAndroidBackgroundColor} />
-				)}
+        {Platform.OS === "android" && (
+          <StatusBar
+            barStyle={config.theme.values.defaultStatusBarStyleAndroid}
+            backgroundColor={
+              config.theme.values.defaultStatusBarAndroidBackgroundColor
+            }
+          />
+        )}
 
-				<Provider store={reduxStore}>
-					<Navigator style={localStyle.container} />
-				</Provider>
-			</View>
-		)
-	}
+        <Provider store={reduxStore}>
+          <Navigator style={localStyle.container} />
+        </Provider>
+      </View>
+    );
+  }
 }
 
 /***********************************************************************************************
@@ -63,25 +61,25 @@ local styling
 ***********************************************************************************************/
 
 const localStyle = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-	},
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
 
 /***********************************************************************************************
 global variables / settings
 ***********************************************************************************************/
 
 // both needed by node-forge for the encryption functionality
-global.Buffer = require('buffer').Buffer
-global.btoa = require('btoa')
+global.Buffer = require("buffer").Buffer;
+global.btoa = require("btoa");
 
 // deactivates the logbox-warning about the debugger running in the background
-LogBox.ignoreLogs(['Remote debugger'])
+LogBox.ignoreLogs(["Remote debugger"]);
 
 /***********************************************************************************************
 export
 ***********************************************************************************************/
 
-export default App
+export default App;
