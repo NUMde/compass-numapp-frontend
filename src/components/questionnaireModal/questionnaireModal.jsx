01f0efb9f75a9367a2a5c7079733fcf14508a51f
Marcel Bastian <m_bastian@outlook.de>
2022-02-08 15:02:00 +0100

    extract 'bottomBar' into own component

--- src/components/questionnaireModal/questionnaireModal.jsx
+++ src/components/questionnaireModal/questionnaireModal.jsx
@@ -192,6 +192,33 @@ class QuestionnaireModal extends Component {
     }
   };
 
+  /**
+   * handles the event triggered by pressing the back button of the modal
+   */
+  handleBackPress = () => {
+    const { actions } = this.props;
+    setAccessibilityResponder(this.modalTitleRef);
+    this.lastPageNavigationWasForwards = false;
+    actions.switchContent(false);
+    this.handleScrollTo({ y: 0, animated: false });
+  };
+
+  /**
+   * handles the event triggered by pressing the middle/forward button of the modal
+   */
+  handleForwardPress = () => {
+    const { actions, categories, currentCategoryIndex, currentPageIndex } =
+      this.props;
+    setAccessibilityResponder(this.modalTitleRef);
+    this.lastPageNavigationWasForwards = true;
+    actions.switchContent(
+      true,
+      categories[currentCategoryIndex].item.length,
+      currentPageIndex,
+    );
+    this.handleScrollTo({ y: 0, animated: false });
+  };
+
   // "getter"
   /*-----------------------------------------------------------------------------------*/
 
@@ -914,207 +941,6 @@ class QuestionnaireModal extends Component {
     );
   };
 
-  /**
-   * creates the bottom-navigation-bar of the modal
-   */
-  createBottomBar = () => {
-    const { currentPageIndex, currentCategoryIndex, categories, actions } =
-      this.props;
-    return (
-      <View
-        style={
-          config.appConfig.useProgressBar
-            ? localStyle.bottomBarWrapper
-            : localStyle.bottomBarWrapperWithShadow
-        }
-      >
-        {config.appConfig.useProgressBar && (
-          <ProgressBar
-            progress={
-              config.appConfig.useStrictModeProgressBar
-                ? exportService.calculatePageProgress(this.props)
-                : currentPageIndex /
-                  categories[currentCategoryIndex].item.length
-            }
-          />
-        )}
-
-        <View style={localStyle.bottomBarButtons}>
-          {/* the left navigational button if we're not on page 1 */}
-          {currentPageIndex > 1 && (
-            <Button
-              type="clear"
-              accessibilityLabel={localization.translate('accessibility').back}
-              accessibilityRole={
-                localization.translate('accessibility').types.button
-              }
-              accessibilityHint={
-                localization.translate('accessibility').questionnaire
-                  .leftButtonHint
-              }
-              onPress={() => {
-                setAccessibilityResponder(this.modalTitleRef);
-                this.lastPageNavigationWasForwards = false;
-                actions.switchContent(false);
-                this.handleScrollTo({ y: 0, animated: false });
-              }}
-              style={localStyle.modalPaginationButton}
-              icon={
-                <Icon
-                  name={I18nManager.isRTL ? 'arrow-right' : 'arrow-left'}
-                  type="material-community"
-                  color={config.theme.colors.accent4}
-                />
-              }
-            />
-          )}
-
-          {/* placeholder for the button on the left side if we're on page 1 */}
-          {currentPageIndex === 1 && (
-            <View
-              style={{
-                ...localStyle.modalPaginationButton,
-                ...localStyle.modalPaginationButtonLeft,
-              }}
-            />
-          )}
-
-          {/* the confirmation button in the middle - its color depends on checkCurrentPageState() */}
-          <Button
-            type="clear"
-            accessibilityLabel={
-              this.checkCurrentPageState()
-                ? localization.translate('accessibility').questionnaire
-                    .middleButtonFinished
-                : localization.translate('accessibility').questionnaire
-                    .middleButtonUnfinished
-            }
-            accessibilityRole={
-              localization.translate('accessibility').types.button
-            }
-            accessibilityHint={
-              localization.translate('accessibility').questionnaire
-                .middleButtonHint
-            }
-            onPress={() => {
-              setAccessibilityResponder(this.modalTitleRef);
-              this.lastPageNavigationWasForwards = true;
-              actions.switchContent(
-                true,
-                categories[currentCategoryIndex].item.length,
-                currentPageIndex,
-              );
-              this.handleScrollTo({ y: 0, animated: false });
-            }}
-            icon={
-              <Icon
-                name="check"
-                reverse
-                type="material-community"
-                color={
-                  this.checkCurrentPageState()
-                    ? config.theme.colors.success
-                    : config.theme.colors.accent4
-                }
-              />
-            }
-          />
-
-          {/* navigational button on the right side - if we're not the last page
-					accessibility: if VoiceOver/TalkBalk is on, we use this button for the closing mechanism,
-					as the middle button can be used to go to the next page. */}
-          {currentPageIndex < categories[currentCategoryIndex].item.length && (
-            <Button
-              type="clear"
-              accessibilityLabel={localization.translate('accessibility').close}
-              accessibilityRole={
-                localization.translate('accessibility').types.button
-              }
-              accessibilityHint={
-                localization.translate('accessibility').questionnaire
-                  .rightButtonHint
-              }
-              onPress={() => {
-                if (!this.isAccessibilityOn) {
-                  setAccessibilityResponder(this.modalTitleRef);
-                  this.lastPageNavigationWasForwards = true;
-                  actions.switchContent(true);
-                  this.handleScrollTo({ y: 0, animated: false });
-                } else {
-                  // when accessibility features are enabled, user should be able to close modal with this button
-                  actions.hideQuestionnaireModal();
-                }
-              }}
-              style={localStyle.modalPaginationButton}
-              icon={
-                <Icon
-                  name={
-                    this.isAccessibilityOn
-                      ? 'close'
-                      : I18nManager.isRTL
-                      ? 'arrow-left'
-                      : 'arrow-right'
-                  }
-                  type="material-community"
-                  color={config.theme.colors.accent4}
-                />
-              }
-            />
-          )}
-
-          {/* placeholder in for the button on the right side - if we're on the last page */}
-          {currentPageIndex ===
-            categories[currentCategoryIndex].item.length && (
-            <View
-              style={{
-                ...localStyle.modalPaginationButton,
-                ...localStyle.modalPaginationButtonRight,
-              }}
-            />
-          )}
-        </View>
-      </View>
-    );
-  };
-
-  /**
-   * creates the modal itself if categories are loaded
-   */
-  createFormContent = () => {
-    const { showQuestionnaireModal, actions, categories } = this.props;
-    return (
-      <View>
-        {categories && (
-          <RNModal
-            scrollOffsetMax={50}
-            avoidKeyboard
-            propagateSwipe
-            backdropOpacity={0.9}
-            style={localStyle.modal}
-            swipeDirection={['down']}
-            scrollTo={this.handleScrollTo}
-            scrollOffset={this.scrollOffset}
-            isVisible={showQuestionnaireModal}
-            onBackdropPress={actions.hideQuestionnaireModal}
-            onSwipeComplete={actions.hideQuestionnaireModal}
-            onBackButtonPress={actions.hideQuestionnaireModal}
-            onModalWillHide={() =>
-              exportService.checkCompletionStateOfMultipleItems(
-                null,
-                this.props,
-              )
-            }
-          >
-            {/* renders the content of the page */}
-            {this.createModalContent()}
-
-            {/* renders the bottom bar */}
-            {this.createBottomBar()}
-          </RNModal>
-        )}
-      </View>
-    );
-  };
   // rendering
   /*-----------------------------------------------------------------------------------*/
 
@@ -1125,12 +951,194 @@ class QuestionnaireModal extends Component {
     // if there is something to render
     // eslint-disable-next-line react/destructuring-assignment
     if (typeof this.props.currentCategoryIndex === 'number') {
-      return this.createFormContent();
+      const {
+        showQuestionnaireModal,
+        actions,
+        categories,
+        currentPageIndex,
+        currentCategoryIndex,
+      } = this.props;
+      return (
+        <RNModal
+          scrollOffsetMax={50}
+          avoidKeyboard
+          propagateSwipe
+          backdropOpacity={0.9}
+          style={localStyle.modal}
+          swipeDirection={['down']}
+          scrollTo={this.handleScrollTo}
+          scrollOffset={this.scrollOffset}
+          isVisible={showQuestionnaireModal}
+          onBackdropPress={actions.hideQuestionnaireModal}
+          onSwipeComplete={actions.hideQuestionnaireModal}
+          onBackButtonPress={actions.hideQuestionnaireModal}
+          onModalWillHide={() =>
+            exportService.checkCompletionStateOfMultipleItems(null, this.props)
+          }
+        >
+          {/* renders the content of the page */}
+          {this.createModalContent()}
+
+          {/* renders the bottom bar */}
+          <BottomBar
+            currentCategoryIndex={currentCategoryIndex}
+            currentPageIndex={currentPageIndex}
+            categories={categories}
+            actions={actions}
+            handleBackPress={this.handleBackPress}
+            handleForwardPress={this.handleForwardPress}
+            completed={this.checkCurrentPageState()}
+            usesAccessibility={this.isAccessibilityOn}
+          />
+        </RNModal>
+      );
     }
     // if not
-
     return <View />;
   }
+}
+
+/**
+ * creates the bottom-navigation-bar of the modal
+ * @param {object} props the props of this component
+ * @param {number} props.currentPageIndex the current page index, i.e. the number of the question within the current category
+ * @param {number} props.currentCategoryIndex the current category index, i.e. the number of the first level index
+ * @param {QuestionnaireItem[]} props.categories the list of all categories, i.e. the first level items
+ * @param {Object<Function>} props.actions
+ * @param {Function } props.handleBackPress handler for the back button
+ * @param {Object<Function>} props.handleForwardPress handler for the forward button
+ * @param {Boolean} props.completed whether the current page is completed
+ * @param {Boolean} props.usesAccessibility whether a screenReader is enabled
+ */
+function BottomBar({
+  currentPageIndex,
+  currentCategoryIndex,
+  categories,
+  actions,
+  handleBackPress,
+  handleForwardPress,
+  completed,
+  usesAccessibility,
+}) {
+  return (
+    <View
+      style={
+        config.appConfig.useProgressBar
+          ? localStyle.bottomBarWrapper
+          : localStyle.bottomBarWrapperWithShadow
+      }
+    >
+      {config.appConfig.useProgressBar && (
+        <ProgressBar
+          progress={
+            config.appConfig.useStrictModeProgressBar
+              ? exportService.calculatePageProgress({
+                  categories,
+                  currentCategoryIndex,
+                  currentPageIndex,
+                })
+              : currentPageIndex / categories[currentCategoryIndex].item.length
+          }
+        />
+      )}
+
+      <View style={localStyle.bottomBarButtons}>
+        {/* the left navigational button; hidden if on page 1 */}
+        <Button
+          type="clear"
+          disabled={currentPageIndex === 1}
+          disabledStyle={localStyle.disabledButton}
+          accessibilityLabel={localization.translate('accessibility').back}
+          accessibilityRole={
+            localization.translate('accessibility').types.button
+          }
+          accessibilityHint={
+            localization.translate('accessibility').questionnaire.leftButtonHint
+          }
+          onPress={handleBackPress}
+          style={localStyle.modalPaginationButton}
+          icon={
+            <Icon
+              name={I18nManager.isRTL ? 'arrow-right' : 'arrow-left'}
+              type="material-community"
+              color={config.theme.colors.accent4}
+            />
+          }
+        />
+        {/* the confirmation button in the middle - its color depends on checkCurrentPageState() */}
+        <Button
+          type="clear"
+          accessibilityLabel={
+            completed
+              ? localization.translate('accessibility').questionnaire
+                  .middleButtonFinished
+              : localization.translate('accessibility').questionnaire
+                  .middleButtonUnfinished
+          }
+          accessibilityRole={
+            localization.translate('accessibility').types.button
+          }
+          accessibilityHint={
+            localization.translate('accessibility').questionnaire
+              .middleButtonHint
+          }
+          onPress={handleForwardPress}
+          icon={
+            <Icon
+              name="check"
+              reverse
+              type="material-community"
+              color={
+                completed
+                  ? config.theme.colors.success
+                  : config.theme.colors.accent4
+              }
+            />
+          }
+        />
+        {/* navigational button on the right side - if we're not the last page
+					accessibility: if VoiceOver/TalkBalk is on, we use this button for the closing mechanism,
+					as the middle button can be used to go to the next page. */}
+        <Button
+          type="clear"
+          accessibilityLabel={localization.translate('accessibility').close}
+          accessibilityRole={
+            localization.translate('accessibility').types.button
+          }
+          accessibilityHint={
+            localization.translate('accessibility').questionnaire
+              .rightButtonHint
+          }
+          onPress={() => {
+            if (!usesAccessibility) {
+              handleForwardPress();
+            } else {
+              // when accessibility features are enabled, user should be able to close modal with this button
+              actions.hideQuestionnaireModal();
+            }
+          }}
+          style={localStyle.modalPaginationButton}
+          icon={
+            <Icon
+              name={
+                usesAccessibility
+                  ? 'close'
+                  : I18nManager.isRTL
+                  ? 'arrow-left'
+                  : 'arrow-right'
+              }
+              type="material-community"
+              color={config.theme.colors.accent4}
+            />
+          }
+          disabled={
+            currentPageIndex === categories[currentCategoryIndex].item.length
+          }
+          disabledStyle={localStyle.disabledButton}
+        />
+      </View>
+    </View>
+  );
 }
 
 /***********************************************************************************************
@@ -1218,6 +1226,10 @@ localStyle = StyleSheet.create({
     textAlign: 'center',
     flexDirection: 'row',
     justifyContent: 'space-between',
+  },
+
+  disabledButton: {
+    opacity: 0,
   },
 
   modalTitle: {
