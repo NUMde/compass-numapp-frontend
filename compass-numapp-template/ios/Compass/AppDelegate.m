cddea9b7996bc431af4edaccb34e22684eee2522
mbastian93 <m_bastian@outlook.de>
2023-03-29 16:48:12 +0200

    bump deps react + react native
    update several dependencies to enable upgrade of react/react-native:
    react/react-test-renderer -> 18-1.0
    react-native -> 0-70.6
    jest/babel-jest -> 29.5.0
    @testing-library/react-native: 12.0.1

--- compass-numapp-template/ios/Compass/AppDelegate.m
+++ compass-numapp-template/ios/Compass/AppDelegate.m
@@ -19,7 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   if ([FIRApp defaultApp] == nil) {
-    [FIRApp configure];
+  // [FIRApp configure];
   }
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge moduleName:@"Compass" initialProperties:nil];
