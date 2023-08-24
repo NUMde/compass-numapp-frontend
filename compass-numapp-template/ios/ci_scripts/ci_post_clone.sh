076336d0833074500ff46e7ce614e40a8b5e89b3
mbastian93 <m_bastian@outlook.de>
2023-08-24 12:49:06 +0200

    add secret

--- compass-numapp-template/ios/ci_scripts/ci_post_clone.sh
+++ compass-numapp-template/ios/ci_scripts/ci_post_clone.sh
@@ -2,14 +2,18 @@
 
 # Install CocoaPods and yarn using Homebrew.
 brew install cocoapods
-brew install node@18
-brew link node@18
-brew install npm
 
-echo "$PWD"
+NODE_VERSION=18
+brew install node@$NODE_VERSION
+brew link node@$NODE_VERSION
+echo 'export PATH="/usr/local/opt/node@18/bin:$PATH"' >> ~/.zshrc
+brew install npm
 
 # Install dependencies
 cd ../../compass-numapp-template
 npm ci
+
 cd ios
+
+echo "$GOOGLE_SERVICES_SECRET" > ./GoogleService-Info.plist
 pod install 
