7c0cf6846276f6fff82b728d251c20ca8a29dc4d
mbastian93 <m_bastian@outlook.de>
2023-08-24 16:51:20 +0200

    next fix

--- compass-numapp-template/ios/ci_scripts/ci_post_clone.sh
+++ compass-numapp-template/ios/ci_scripts/ci_post_clone.sh
@@ -15,8 +15,10 @@ brew install npm
 cd ../..
 npm ci
 cd ./src/config && mv appConfig.sample.js appConfig.js && mv devConfig.sample.js devConfig.js && mv theme.sample.js theme.js
+cd ..
+mkdir ./assets/files && curl https://raw.githubusercontent.com/NUMde/compass-implementation-guide/master/input/questionnaire-generic.json > ./assets/files/questionnaire.json
 
-cd ../../ios 
+cd ../ios 
 
 echo "$GOOGLE_SERVICES_SECRET" > ./GoogleService-Info.plist
 pod install 
